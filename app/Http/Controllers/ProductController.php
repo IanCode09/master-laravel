4ada7fef7bc2a28960877a118774b740a6774bce
IanCode09 <ianlombu@protonmail.com>
2020-10-20 01:56:57 +0700

    Read and Select Data with Eloquent ORM

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,14 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Support\Facades\DB;
+
+use App\Product;
 
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index() {
-        $products = DB::table('products')->get();
+        $products = Product::all();
         dd($products);
         return view('products.index');
     }
@@ -20,8 +21,7 @@ class ProductController extends Controller
 
     }
     public function show($product) {
-        $product = DB::table('products')->where('id', $product)->first();
-        // $product = DB::table('products')->find($product);
+        $product = Product::findOrFail($product);
         dd($product);
         return view('products.show');
     }
