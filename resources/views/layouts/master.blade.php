a618fe78f7364174b32c9fd7d942cbfd65985b89
IanCode09 <ianlombu@protonmail.com>
2020-10-20 14:46:31 +0700

    Displaying validation errors

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,7 +7,16 @@
     <title>Document</title>
 </head>
 <body>
-    @dump($errors)
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     @if (session('error'))
         <div class="alert alert-danger">
             {{ session('error') }}
